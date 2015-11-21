c5fd21b4f6350c830632f043c28d0c1ddb13aed3
Rebecca <h4xnoodle@gmail.com>
2015-11-21 12:31:18 -0500

    GET, PUT, Login working

--- controllers/doctors/index.js
+++ controllers/doctors/index.js
@@ -3,20 +3,41 @@ var Doctor = require('../../models/doctor');
 
 module.exports = function (router) {
 
-    router.get('/:email', function (req, res) {
-        
+    router.get('/:id', function (req, res) {
+        Doctor.findById(req.params.id, function (err, result) {
+            if (err || !result) {
+                res.status(404).json({'error': 'Doctor not found.'});
+            } else {
+                res.status(200).json(result);
+            }
+        });
     });
 
-    router.put('/:id/update', function(req, res) {
-        res.send("update");
+    router.put('/:id', function(req, res) {
+        delete req.body._id;
+        delete req.body.__v;
+        Doctor.findByIdAndUpdate(req.params.id, req.body, function(err, result) {
+            if (err || !result) {
+                res.status(400).json({'error': 'Update failed.'});
+            } else {
+                res.status(200).json({'success': 'Doctor updated.'});
+            }
+        });
     });
 
-    router.get('/list', function (req, res) {
-        res.send("index");
+    router.post('/login', function (req, res) {
+        Doctor.findOne({email: req.body.email}, function (err, result) {
+            if (err || !result) {
+                res.status(404).json({'error': 'Doctor not found. Register!'});
+            } else if (result.get('password') !== req.body.password) {
+                res.status(401).json({'error': 'Password not be matching yo'});
+            } else {
+                res.status(200).json({'success': 'Logged in yay', 'doctor': result});
+            }
+        });
     });
 
     router.post('/register', function (req, res) {
-
         var newDoctor = new Doctor({
           name: req.body.name,
           password: req.body.password,
