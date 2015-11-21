c23cf142733ef31e2b2f6f402c0e7eb5a1baa688
Rebecca <h4xnoodle@gmail.com>
2015-11-21 16:38:49 -0500

    Find case by ID, and messages by case

--- controllers/cases/index.js
+++ controllers/cases/index.js
@@ -1,23 +1,41 @@
 'use strict';
+var Case = require('../../models/case');
+var Message = require('../../models/message');
 
 module.exports = function (router) {
 
     router.get('/:id', function (req, res) {
-
-        res.send("index");
-
+        Case.findById(req.params.id)
+            .then(function(result) {
+                if (result) {
+                    res.status(200).json(result);
+                } else {
+                    res.status(404).json({'error': 'No case by that id.'});
+                }
+            })
+            .catch(function(error) {
+                res.status(500).json({'error': error});
+            });
     });
 
-    router.get('/doctor/:doctorID/list', function (req, res) {
-
-        res.send("index");
-
-    });
-
-    router.get('/patient/:patientID/list', function (req, res) {
-
-        res.send("index");
-
+    router.get('/:id/messages', function(req, res) {
+        Case.findById(req.params.id)
+            .then(function(result) {
+                if (result) {
+                    result.messages()
+                        .then(function(results) {
+                            res.json(results);
+                        })
+                        .catch(function(error) {
+                            res.status(500).json({'error': error});
+                        });
+                } else {
+                    res.status(404).json({'error': 'No case with that ID, so no messages for you!'});
+                }
+            })
+            .catch(function(error) {
+                res.status(500).json({'error': error});
+            })
     });
 
     router.post('/modify', function (req, res) {
